6605507c68eb7bd987c5a568932b165bd82c1f4f
Alec Cove <alec.cove@gmail.com>
2016-10-18 20:16:15 -0400

    Complete Problem 5

--- src/Problem5.scala
+++ src/Problem5.scala
@@ -5,15 +5,33 @@
  *
  * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
  *
- * Solution:
+ * Solution: 232792560
  */
 object Problem5 {
 
 
     /*
-     *
+     * Start at the highest of the divisors as the smallest possible number, divide by each number decrementing
      */
     def main(args: Array[String]) {
 
+        val start:Double = 20
+        var upperBound: Double = start
+        var smallestPossible:Double = start
+
+        while (upperBound > 0) {
+            val quotient:Double = smallestPossible / upperBound
+            if (! isInt(quotient)) {
+                smallestPossible += 1
+                upperBound = start
+            }
+            upperBound -= 1
+        }
+        println(smallestPossible.toInt);
+    }
+
+
+    def isInt(value:Double):Boolean = {
+        value % 1 == 0
     }
 }
