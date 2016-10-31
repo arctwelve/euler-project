900cfee7658e3e9c9918933d460af698f9b64dc0
Alec Cove <alec.cove@gmail.com>
2016-10-31 16:02:56 -0400

    Start Problem 7

---
+++ src/Problem7.py
@@ -0,0 +1,39 @@
+# By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+#
+# What is the 10,001st prime number?
+#
+# Solution:
+#
+
+
+primeSieve = []
+primes = []
+
+
+def is_prime(val):
+    if val < 2:
+        return False
+    for i in range(2, val):
+        if val % i == 0:
+            return False
+
+    return True
+
+
+def sieve_primes(amount):
+
+    for i in range(amount):
+        primeSieve.append(0)
+
+    for i in range(amount):
+        if primeSieve[i] == 0 and is_prime(i):
+            primeSieve[i] = 1
+            primes.append(i)
+            for x in range(i * 2, amount, i):
+                primeSieve[x] = -1
+
+
+candidates = 22000
+sieve_primes(candidates)
+
+print(len(primes))
