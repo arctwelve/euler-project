fb1b0e3c377a4f9039a50d2e1f5f661f930c7e3b
Alec Cove <alec.cove@gmail.com>
2016-11-17 20:45:56 -0500

    Start Problem 9

--- src/Python9.py
+++ src/Python9.py
@@ -15,3 +15,22 @@
 #
 # Solution:
 #
+
+
+# iterate through a and b to find triplets. see if the square of the sum is a natural number
+import math
+
+
+def build_prime_triplets(high_range):
+    triplets = []
+    for a in range(1, high_range):
+        for b in range(a + 1, high_range):
+            c = math.sqrt(a * a + b * b)
+            if c % 1 == 0:
+                triplet = [a, b, int(c)]
+                triplets.append(triplet)
+
+    return triplets
+
+
+print(build_prime_triplets(20))
