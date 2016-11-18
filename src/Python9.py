63f883e05c3965d10ab2fd5cef151d4e92b24c83
Alec Cove <alec.cove@gmail.com>
2016-11-17 22:33:45 -0500

    Complete Problem 9

--- src/Python9.py
+++ src/Python9.py
@@ -22,17 +22,15 @@ import math
 
 # iterate through a and b to find triplets. if the square of the sum of a + b is a
 # natural number then the triplet is Pythagorean. Find the sum that equals 1000.
-def build_prime_triplets(mxa):
+def prime_triplets_product(ubound):
 
-    triplet = 0
-    for a in range(1, mxa):
-        for b in range(a + 1, mxa):
+    for a in range(1, ubound):
+        for b in range(a + 1, ubound):
             c = math.sqrt(a * a + b * b)
             if c % 1 == 0:
                 if a + b + c == 1000:
-                    triplet = [a, b, int(c)]
-                    break
+                    return a * b * int(c)
 
-    return triplet[0] * triplet[1] * triplet[2]
+    return 0
 
-print(build_prime_triplets(380))
+print(prime_triplets_product(380))
